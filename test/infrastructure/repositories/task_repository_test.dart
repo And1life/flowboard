9d5505f66c6ef025773b21b73c681853c9d73056
Anatoly <dmitriev18.dev@gmail.com>
2026-09-21 11:11:33 +0300

    test: complete task repository coverage

--- test/infrastructure/repositories/task_repository_test.dart
+++ test/infrastructure/repositories/task_repository_test.dart
@@ -27,5 +27,64 @@ void main() {
       final result = repository.findById(999);
       expect(result, isNull);
     });
+
+    test('returns all tasks', () {
+      final repository = TaskRepository();
+
+      final task1 = Task(
+        id: 2,
+        title: 'Task 1',
+        description: 'Description 1',
+      );
+
+      final task2 = Task(
+        id: 3,
+        title: 'Task 2',
+        description: 'Description 2',
+      );
+
+      repository.create(task1);
+      repository.create(task2);
+
+      final result = repository.findAll();
+
+      expect(result, equals([task1, task2]));
+    });
+
+    test('delete a task', () {
+      final repository = TaskRepository();
+
+      final task = Task(
+        id: 4,
+        title: 'Task 4',
+        description: 'Description 4',
+      );
+
+      repository.create(task);
+
+      expect(repository.findById(4), equals(task));
+
+      repository.delete(4);
+
+      expect(repository.findById(4), isNull);
+    });
+
+    test('does not expose internal task list', () {
+      final repository = TaskRepository();
+
+      final task = Task(
+        id: 5,
+        title: 'Task 5',
+        description: 'Descroption 5',
+      );
+
+      repository.create(task);
+
+      final tasks = repository.findAll();
+
+      tasks.clear();
+
+      expect(repository.findById(5), equals(task));
+    });
   });
 }
